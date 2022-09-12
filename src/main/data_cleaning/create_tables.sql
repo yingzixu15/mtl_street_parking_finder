ce1e50b86625fd1cfcd51b40777cbc298937932b
yingzixu15 <39885738+yingzixu15@users.noreply.github.com>
2022-09-11 22:21:47 -0400

    load voie and line segments

--- src/main/data_cleaning/create_tables.sql
+++ src/main/data_cleaning/create_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS signs(id INTEGER PRIMARY KEY, longitude REAL NOT NULL, latitude REAL NOT NULL, nom_arrond TEXT, description_rpa TEXT, start_month INTEGER NOT NULL, start_date INTEGER NOT NULL, end_month INTEGER NOT NULL, end_date INTEGER NOT NULL);
 
-CREATE TABLE IF NOT EXISTS voie(id_voie INTEGER PRIMARY KEY, nom_voie TEXT NOT NULL, nom_ville TEXT, sens_cir INTEGER, type_f TEXT) WITHOUT ROWID;
+CREATE TABLE IF NOT EXISTS voie(id_voie INTEGER PRIMARY KEY, nom_voie TEXT NOT NULL, nom_ville TEXT, sens_cir INTEGER, type_f TEXT);
 CREATE INDEX index_voie_nom ON voie (nom_voie);
 CREATE TABLE IF NOT EXISTS line_segments(
     id INTEGER PRIMARY KEY, string_type TEXT, coordinates TEXT NOT NULL, 
--- src/main/data_cleaning/load_db.py
+++ src/main/data_cleaning/load_db.py
@@ -4,7 +4,6 @@ import json
 
 
 def main():
-    # connect to DB
     directory_path = os.getcwd()
     dbpath = os.path.join(directory_path, 'mtl_parking.db')
     connection = sqlite3.connect(dbpath)
@@ -12,6 +11,7 @@ def main():
 
     load_signs(connection, cursor, directory_path)
     load_voie(connection, cursor, directory_path)
+    load_line_segments(connection, cursor, directory_path)
 
     connection.close()
 
@@ -34,7 +34,42 @@ def load_signs(connection: sqlite3.Connection, cursor: sqlite3.Cursor, directory
 
 
 def load_voie(connection: sqlite3.Connection, cursor: sqlite3.Cursor, directory_path: str):
-    pass
+    def map_feature(feature: dict):
+        property = feature['properties']
+        if 'TYPE_F' not in property:
+            property['TYPE_F'] = None
+        if 'NOM_VILLE' not in property:
+            property['NOM_VILLE'] = None
+        return (property['ID_VOIE'], property['NOM_VOIE'], property['NOM_VILLE'], property['SENS_CIR'], property['TYPE_F'])
+    
+    data_path = os.path.join(directory_path, 'data', 'gbdouble.json')
+    with open(data_path, 'r') as read_file:
+        features = json.load(read_file)['features']
+        voie_list = map(lambda row: map_feature(row), features)
+        cursor.executemany('INSERT OR IGNORE INTO voie VALUES (?,?,?,?,?)', voie_list)
+        connection.commit()
+        
+        cursor.execute('SELECT COUNT(*) FROM voie')
+        num_rows_added = cursor.fetchone()
+        print(f'Number of rows in voie after loading: {num_rows_added[0]}')
+        
+        
+        
+def load_line_segments(connection: sqlite3.Connection, cursor: sqlite3.Cursor, directory_path: str):
+    def map_feature(feature: dict):
+        return (feature['geometry']['type'], json.dumps(feature['geometry']['coordinates']), feature['properties']['ID_VOIE'])
+        
+    data_path = os.path.join(directory_path, 'data', 'gbdouble.json')
+    with open(data_path, 'r') as read_file:
+        items = json.load(read_file)['features']
+        items = map(lambda row: map_feature(row), filter(lambda row: row['geometry'] is not None, items))
+        cursor.executemany('INSERT INTO line_segments VALUES (NULL,?,?,?)', items)
+        connection.commit()
+        
+        cursor.execute('SELECT COUNT(*) FROM line_segments')
+        num_rows_added = cursor.fetchone()
+        print(f'Number of rows in line_segments after loading: {num_rows_added[0]}')
+        
 
 if __name__ == '__main__':
     main()
